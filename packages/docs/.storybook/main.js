d08413362a6e49bd72051e740087a53efefae1b7
ufpj <eng.ubaldo@gmail.com>
2023-04-27 16:35:48 -0300

    teste

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,7 +19,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system/'
+      config.base = 'design-system/'
     }
     return config
   }
